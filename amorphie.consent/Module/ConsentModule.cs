c2bfd241feec4aea9112bd2be106d5b869960cb5
mt <mehmet.tosun@outlook.com>
2023-07-21 15:37:59 +0300

    Added some comments Added new routes getting consent and consent definition

--- amorphie.consent/Module/ConsentModule.cs
+++ amorphie.consent/Module/ConsentModule.cs
@@ -24,8 +24,15 @@ public class ConsentModule : BaseBBTRoute<ConsentDTO, Consent, ConsentDbContext>
         base.AddRoutes(routeGroupBuilder);
         routeGroupBuilder.MapPost("/saveConsentData", SaveConsentDataToDatabase);
         routeGroupBuilder.MapGet("/search", SearchMethod);
-
+        routeGroupBuilder.MapGet("/user/{userId}/consentType/{consentType}", GetUserConsent);
     }
+
+    protected async Task<IResult> GetUserConsent(HttpContext context)
+    {
+        // TODO : Get user consent with id and consent type
+        return Results.Ok();
+    }
+
     protected async Task<IResult> SaveConsentDataToDatabase(ConsentDataDto consentData, [FromServices] ConsentDbContext context, [FromServices] IMapper mapper)
     {
         using var transaction = await context.Database.BeginTransactionAsync();
@@ -54,9 +61,9 @@ public class ConsentModule : BaseBBTRoute<ConsentDTO, Consent, ConsentDbContext>
                 Permission = consentData.Permission
             };
 
-            context.Set<ConsentDefinition>().Add(consentDefinition);
-            context.Set<Consent>().Add(consent);
-            context.Set<ConsentPermission>().Add(consentPermission);
+            context.Add<ConsentDefinition>(consentDefinition);
+            context.Add<Consent>(consent);
+            context.Add<ConsentPermission>(consentPermission);
 
             await context.SaveChangesAsync();
             await transaction.CommitAsync();
