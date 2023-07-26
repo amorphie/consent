f7183a3a6adc30e75f320106f84bff8203784672
yarasaa <mehmet.akbaba2011@gmail.com>
2023-07-26 10:30:02 +0300

    update

--- amorphie.consent/Program.cs
+++ amorphie.consent/Program.cs
@@ -3,6 +3,7 @@ using amorphie.consent.Validator;
 using amorphie.core.Extension;
 using amorphie.core.HealthCheck;
 using amorphie.core.Identity;
+using amorphie.core.security.Extensions;
 using amorphie.core.Swagger;
 using amorphie.template.HealthCheck;
 using FluentValidation;
@@ -20,6 +21,8 @@ builder.Services.AddScoped<IBBTIdentity, FakeIdentity>();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+await builder.Configuration.AddVaultSecrets("amorphie-consent", new string[] { "amorphie-consent" });
+var postgreSql = builder.Configuration["PostgreSql"];
 builder.Services.AddSwaggerGen(options=>
 {
     options.OperationFilter<AddSwaggerParameterFilter>();
@@ -29,10 +32,11 @@ builder.Services.AddSwaggerGen(options=>
 builder.Services.AddValidatorsFromAssemblyContaining<ConsentValidator>(includeInternalTypes: true);
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
-
 builder.Services.AddDbContext<ConsentDbContext>
-    // (options => options.UseInMemoryDatabase("TemplateDbContext"));
-    (options => options.UseNpgsql("Host=localhost:5432;Database=ConsentDb;Username=postgres;Password=postgres", b => b.MigrationsAssembly("amorphie.consent.data")));
+    (options => options.UseNpgsql(postgreSql, b => b.MigrationsAssembly("amorphie.consent.data")));
+// builder.Services.AddDbContext<ConsentDbContext>
+//     // (options => options.UseInMemoryDatabase("TemplateDbContext"));
+//     (options => options.UseNpgsql("Host=localhost:5432;Database=ConsentDb;Username=postgres;Password=postgres", b => b.MigrationsAssembly("amorphie.consent.data")));
 
 
 var app = builder.Build();
--- vault.sh
+++ vault.sh
@@ -1,2 +1,2 @@
 sleep 5 &&
-curl -X POST 'http://bbt-template-vault:8200/v1/secret/data/amorphie-template' -H "Content-Type: application/json" -H "X-Vault-Token: admin" -d '{ "data": {"pass": "my-password", "username":"my-username"} }'
+curl -X POST 'http://localhost:8200/v1/secret/data/amorphie-consent' -H "Content-Type: application/json" -H "X-Vault-Token: admin" -d '{ "data": {"PostgreSql":Host=localhost:5432;Database=ConsentDb;Username=postgres;Password=postgres} }'
