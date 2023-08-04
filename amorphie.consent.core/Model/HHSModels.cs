cd0c7eb93e855e3ae58da07539c9dc03b461685a
yarasaa <mehmet.akbaba2011@gmail.com>
2023-08-04 15:45:10 +0300

    add hhs test models and puml

---
+++ amorphie.consent.core/Model/HHSModels.cs
@@ -0,0 +1,110 @@
+public class HesapHizmetiSağlayıcı
+{
+    public string HhsKod { get; set; }
+    public string YosKod { get; set; }
+}
+
+public class GucLuKimlikDogrulama
+{
+    public string YetYntm { get; set; }
+    public string YonAdr { get; set; }
+    public string BldAdr { get; set; }
+    public string HhsYonAdr { get; set; }
+    public DateTime YetTmmZmn { get; set; }
+}
+
+public class IzinBilgisi
+{
+    public string[] IzinTur { get; set; }
+    public DateTime ErisimIzniSonTrh { get; set; }
+    public DateTime? HesapIslemBslZmn { get; set; }
+    public DateTime? HesapIslemBtsZmn { get; set; }
+}
+
+public class HesapBilgisiRizaBilgisi
+{
+    public string RizaNo { get; set; }
+    public DateTime OlusZmn { get; set; }
+    public DateTime GnclZmn { get; set; }
+    public string RizaDrm { get; set; }
+    public string RizaIptDtyKod { get; set; }
+}
+
+public class Kimlik
+{
+    public string OhkTur { get; set; }
+    public string KimlikTur { get; set; }
+    public string KimlikVrs { get; set; }
+}
+
+public class KatilimciBilgisi
+{
+    public string HhsKod { get; set; }
+    public string YosKod { get; set; }
+}
+public class AyrintiBilgi
+{
+    public string OhkMsj { get; set; }
+}
+
+public class HesapBilgisiRizaIstegi
+{
+    public HesapBilgisiRizaBilgisi RzBlg { get; set; }
+    public Kimlik Kmlk { get; set; }
+    public KatilimciBilgisi KatilimciBlg { get; set; }
+    public GucLuKimlikDogrulama Gkd { get; set; }
+    public HesapHizmetiSağlayıcı HspBlg { get; set; }
+    public IzinBilgisi IznBlg { get; set; }
+}
+
+public class HesapBilgisiRizaIstegiResponse
+{
+    public HesapBilgisiRizaBilgisi RzBlg { get; set; }
+    public Kimlik Kmlk { get; set; }
+    public KatilimciBilgisi KatilimciBlg { get; set; }
+    public GucLuKimlikDogrulama Gkd { get; set; }
+    public HesapHizmetiSağlayıcı HspBlg { get; set; }
+    public IzinBilgisi IznBlg { get; set; }
+    public AyrintiBilgi AyrintiBlg { get; set; }
+}
+
+// public class HesapBilgisiRizaBilgisiDto
+// {
+//     public string RizaNo { get; set; }
+//     public DateTime OlusZmn { get; set; }
+//     public DateTime GnclZmn { get; set; }
+//     public string RizaDrm { get; set; }
+//     public string RizaIptDtyKod { get; set; }
+// }
+
+// public class KimlikDto
+// {
+//     public string KimlikTur { get; set; }
+//     public string KimlikVrs { get; set; }
+//     public string KrmKmlkTur { get; set; }
+//     public string KrmKmlkVrs { get; set; }
+//     public string OhkTur { get; set; }
+// }
+
+// public class KatilimciBilgisiDto
+// {
+//     public string HhsKod { get; set; }
+//     public string YosKod { get; set; }
+// }
+
+// public class GucLuKimlikDogrulamaDto
+// {
+//     public string YetYntm { get; set; }
+//     public string YonAdr { get; set; }
+//     public string BldAdr { get; set; }
+//     public string HhsYonAdr { get; set; }
+//     public DateTime YetTmmZmn { get; set; }
+// }
+
+// public class IzinBilgisiDto
+// {
+//     public string[] IzinTur { get; set; }
+//     public DateTime ErisimIzniSonTrh { get; set; }
+//     public DateTime? HesapIslemBslZmn { get; set; }
+//     public DateTime? HesapIslemBtsZmn { get; set; }
+// }
