65647a9abd8a43c7be98766a73f2dab0e91c5015
yarasaa <mehmet.akbaba2011@gmail.com>
2023-08-11 11:29:53 +0300

    update

--- redis.sh
+++ redis.sh
@@ -1,6 +1,5 @@
 redis-server --daemonize yes && sleep 3
 redis-cli MSET config-amorphie-contract-db 'User ID=postgres;Password=postgres;Host=Localhost;Port=5432;Database=contract;'
-redis-cli MSET config-error '"en" '{"Errors.NotFound":"The requested item was not found.","Errors.ValidationError":"Validation failed for the provided data."}'
 redis-cli save 
 redis-cli shutdown 
 redis-server
