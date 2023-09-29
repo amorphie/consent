92aeebab809d9cc08460dd5d8897cc0105faa4ae
“ozlem-hb” <ozlemhancioglu@live.com>
2023-09-29 12:55:18 +0300

    namespace usage changed

--- amorphie.consent.core/DTO/OpenBanking/RequestHeaderDto.cs
+++ amorphie.consent.core/DTO/OpenBanking/RequestHeaderDto.cs
@@ -1,14 +1,10 @@
-namespace amorphie.consent.core.DTO.OpenBanking
+namespace amorphie.consent.core.DTO.OpenBanking;
+public class RequestHeaderDto
 {
-    public class RequestHeaderDto
-    {
-        public string ContentType { get; set; }
-        public string XRequestID { get; set; }
-        public string XGroupID { get; set; }
-        public string XASPSPCode { get; set; }
-        public string XTPPCode { get; set; }
-        public string PSUInitiated { get; set; }
-
-    }
-
+    public string ContentType { get; set; }
+    public string XRequestID { get; set; }
+    public string XGroupID { get; set; }
+    public string XASPSPCode { get; set; }
+    public string XTPPCode { get; set; }
+    public string PSUInitiated { get; set; }
 }
