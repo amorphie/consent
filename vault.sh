b4f1f4154ab90ccd990e3cfef5e765ee2efba61b
yarasaa <mehmet.akbaba2011@gmail.com>
2023-09-29 16:01:43 +0300

    vault initial data update

--- vault.sh
+++ vault.sh
@@ -1,2 +1,2 @@
 sleep 5 &&
-curl -X POST 'http://bbt-consent-vault:8200/v1/secret/data/amorphie-consent' -H "Content-Type: application/json" -H "X-Vault-Token: admin" -d '{ "data": {"pass": "my-password", "username":"my-username","PostgreSql":"Host=localhost:5432;Database=ConsentDb;Username=postgres;Password=postgres"} }'
+curl -X POST 'http://bbt-consent-vault:8200/v1/secret/data/amorphie-consent' -H "Content-Type: application/json" -H "X-Vault-Token: admin" -d '{ "data": {"HHSCode": "0125", "HHSForwardingAddress":"https://test-amorphie-consent.burgan.com.tr/swagger/index.html","PostgreSql":"Host=localhost:5432;Database=ConsentDb;Username=postgres;Password=postgres"} }'
