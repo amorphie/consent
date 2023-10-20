d471f754a2dabfcba7cf1f417e3156c19ea71ba8
yarasaa <mehmet.akbaba2011@gmail.com>
2023-10-20 12:54:17 +0300

    url mask

--- amorphie.consent/Module/YosInfoModule.cs
+++ amorphie.consent/Module/YosInfoModule.cs
@@ -2,6 +2,9 @@ using amorphie.consent.core.DTO;
 using amorphie.consent.core.Model;
 using amorphie.consent.data;
 using amorphie.core.Module.minimal_api;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 public class YosInfoModule : BaseBBTRoute<YosInfoDto, YosInfo, ConsentDbContext>
 {
@@ -15,5 +18,31 @@ public class YosInfoModule : BaseBBTRoute<YosInfoDto, YosInfo, ConsentDbContext>
     public override void AddRoutes(RouteGroupBuilder routeGroupBuilder)
     {
         base.AddRoutes(routeGroupBuilder);
+        routeGroupBuilder.MapGet("/yosId/{yosId}", GetYosWithYosId);
+    }
+
+
+
+
+    protected async Task<IResult> GetYosWithYosId(
+     [FromServices] ConsentDbContext context,
+     IMapper mapper,
+     string yosId
+ )
+    {
+        var yosInfo = await context.Set<YosInfo>()
+            .AsNoTracking()
+            .Where(x => x.kod == yosId)
+            .FirstOrDefaultAsync();
+
+        if (yosInfo != null)
+        {
+           var data= mapper.Map<YosInfoDto>(yosInfo);
+            return Results.Ok(data);
+        }
+        else
+        {
+            return Results.NotFound("YosInfo not found");
+        }
     }
 }
