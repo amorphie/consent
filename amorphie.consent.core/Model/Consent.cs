41e675a16ca13038864836f2453acb3a7f9fee07
“ozlem-hb” <ozlemhancioglu@live.com>
2023-11-20 15:39:03 +0300

    Consent entity ScopeId, RoleId, ClientId, Variant columns added

--- amorphie.consent.core/Model/Consent.cs
+++ amorphie.consent.core/Model/Consent.cs
@@ -5,6 +5,10 @@ using NpgsqlTypes;
 public class Consent : EntityBase
 {
     public Guid? UserId { get; set; }
+    public Guid? ScopeId { get; set; }
+    public Guid? RoleId { get; set; }
+    public Guid ClientId { get; set; }
+    public string? Variant { get; set; }
     public string State { get; set; }
     public string? Description { get; set; }
     public string? xGroupId { get; set; }
---
+++ amorphie.consent.data/Migrations/20231120121150_Task193504ConsentAddColumns.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace amorphie.consent.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Task193504ConsentAddColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ClientId",
+                table: "Consents",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RoleId",
+                table: "Consents",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ScopeId",
+                table: "Consents",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Variant",
+                table: "Consents",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Consents");
+
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "Consents");
+
+            migrationBuilder.DropColumn(
+                name: "ScopeId",
+                table: "Consents");
+
+            migrationBuilder.DropColumn(
+                name: "Variant",
+                table: "Consents");
+        }
+    }
+}
