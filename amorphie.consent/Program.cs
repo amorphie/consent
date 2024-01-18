6864b1bf36666e62e2a5226e77afc84c256cb423
yarasaa <mehmet.akbaba2011@gmail.com>
2024-01-18 11:50:27 +0300

    update

--- amorphie.consent/Program.cs
+++ amorphie.consent/Program.cs
@@ -47,6 +47,7 @@ builder.Services.AddEndpointsApiExplorer();
 await builder.Configuration.AddVaultSecrets("amorphie-consent", new string[] { "amorphie-consent" });
 var postgreSql = builder.Configuration["PostgreSql"];
 var pfxPassword=builder.Configuration["PfxPassword"];
+Console.WriteLine($"PostgreSql: {postgreSql}");
 string jsonFilePath = Path.Combine(AppContext.BaseDirectory, "test.json");
 
 builder.Services.AddSwaggerGen(options =>
