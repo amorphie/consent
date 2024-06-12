8026dd690acbe0c41785287aa5a8e502a8d2321d
“ozlem-hb” <ozlemhancioglu@live.com>
2024-06-13 00:22:33 +0300

    UI Get account, payment consent dtos updated to list customer information.

--- amorphie.consent.core/DTO/OpenBanking/HHS/Consent/HHSAccountConsentDto.cs
+++ amorphie.consent.core/DTO/OpenBanking/HHS/Consent/HHSAccountConsentDto.cs
@@ -9,4 +9,6 @@ namespace amorphie.consent.core.DTO.OpenBanking.HHS;
 public class HHSAccountConsentDto : HHSConsentDto
 {
     public HesapBilgisiRizasiHHSDto AdditionalData { get; set; }
+    public string? CustomerNumber { get; set; }
+    public string? InstitutionCustomerNumber { get; set; }
 }
