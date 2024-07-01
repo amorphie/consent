5df4f9b22def1bb96e30395c59ee903d86353a64
“ozlem-hb” <ozlemhancioglu@live.com>
2024-07-01 15:48:50 +0300

    Balance change kafka changes reflected to code.

--- amorphie.consent/Module/OpenBankingHhsConsentModule.cs
+++ amorphie.consent/Module/OpenBankingHhsConsentModule.cs
@@ -1877,7 +1877,7 @@ public class OpenBankingHHSConsentModule : BaseBBTRoute<OpenBankingConsentDto, C
             BalanceChangedKafkaRecordDto? kafkaRecord = await httpContext.Deserialize<BalanceChangedKafkaRecordDto>();
             if (kafkaRecord is { message.data: not null }
                 && !string.IsNullOrEmpty(kafkaRecord.message.data.OPEN_BANKING_CONSENT_NUMBER)
-                && !string.IsNullOrEmpty(kafkaRecord.message.data.HESAP_REF)
+                && !string.IsNullOrEmpty(kafkaRecord.message.data.hspRef)
                 && !string.IsNullOrEmpty(kafkaRecord.message.data.INSTANT_BALANCE_NOTIFICATION_PERMISSION)
                 && kafkaRecord.message.data.INSTANT_BALANCE_NOTIFICATION_PERMISSION == OpenBankingConstants.BalanceChangedServiceYesNo.Yes
                 && !string.IsNullOrEmpty(kafkaRecord.message.data.OPEN_BANKING_SHARE_PERMISSION)
@@ -1886,7 +1886,7 @@ public class OpenBankingHHSConsentModule : BaseBBTRoute<OpenBankingConsentDto, C
                 //Get active account consent
                 var getConsentResult = await authorizationService.GetAuthUsedAccountConsent(
                     kafkaRecord.message.data.OPEN_BANKING_CONSENT_NUMBER,
-                    kafkaRecord.message.data.HESAP_REF,
+                    kafkaRecord.message.data.hspRef,
                     new List<string>() { OpenBankingConstants.IzinTur.AnlikBakiyeBildirimi });
 
                 if (!getConsentResult.Result)
@@ -1905,7 +1905,7 @@ public class OpenBankingHHSConsentModule : BaseBBTRoute<OpenBankingConsentDto, C
                 {//Do event process
                     await obEventService.DoEventProcess(consent.Id.ToString(), new KatilimciBilgisiDto() { hhsKod = consentDetail.HhsCode, yosKod = consentDetail.YosCode },
                         OpenBankingConstants.OlayTip.KaynakGuncellendi, OpenBankingConstants.KaynakTip.Bakiye,
-                        kafkaRecord.message.data.HESAP_REF);
+                        kafkaRecord.message.data.hspRef);
                 }
 
             }
