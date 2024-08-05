15c40f606481be78934ec04b495add231c7f8053
“ozlem-hb” <ozlemhancioglu@live.com>
2024-08-05 15:40:27 +0300

    AuthorizationModule CheckAuthorizationForLogin method parameter changed from ScopeTCKN to Scope CheckConsent  method parameter changed from ScopeTCKN to Scope ConsentModule CancelLoginConsent parameter CancelLoginConsentDto property changed from ScopeTCKN to Scope

--- amorphie.consent.core/DTO/Authorization/SaveConsentDto.cs
+++ amorphie.consent.core/DTO/Authorization/SaveConsentDto.cs
@@ -5,7 +5,7 @@ public class SaveConsentDto
     public Guid RoleId { get; set; }
     public string ClientCode { get; set; }
     public long UserTCKN { get; set; }
-    public long ScopeTCKN { get; set; }
+    public string Scope { get; set; }
     public string ConsentType { get; set; }
     public DateTime? LastValidAccessDate { get; set; }
 }
